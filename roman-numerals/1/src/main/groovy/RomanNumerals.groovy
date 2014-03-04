8fcbc390fa15acee0b32ac51651ad13335a01249
mcornell <mcornell74@gmail.com>
2014-03-03 23:12:32 -0500

    all the symbols

--- roman-numerals/1/src/main/groovy/RomanNumerals.groovy
+++ roman-numerals/1/src/main/groovy/RomanNumerals.groovy
@@ -24,9 +24,13 @@ class RomanNumerals {
 
 	def ROMAN_TO_ARABIC = [
 		'M'  : 1000,
+		'CM' : 900,
 		'D'  : 500,
+		'CD' : 400,
 		'C'  : 100,
+		'XC' : 90,
 	    'L'  : 50,
+	    'XL' : 40,
 		'X'  : 10,
 		'IX' : 9,
 		'V'  : 5,
