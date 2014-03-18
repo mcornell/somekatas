59644f21b17308dcdccec108830f2e4498901924
mcornell <mcornell74@gmail.com>
2014-03-17 21:34:29 -0400

    test 3 refactor

--- bowling-game/2/src/BowlingGame.js
+++ bowling-game/2/src/BowlingGame.js
@@ -1,11 +1,18 @@
 function BowlingGame() {
   this.score = 0;
+  this.rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  this.currentRoll = 0;
 }
 
 BowlingGame.prototype.roll = function(pins) {
   this.score += pins;
+  this.rolls[this.currentRoll++] = pins;
 };
 
 BowlingGame.prototype.getScore = function() {
-  return this.score;
+  var score = 0;
+  for (var i = 0; i < this.rolls.length; i++) {
+    score += this.rolls[i];
+  }
+  return score;
 };
