f1feac93f9f3bd9219f9e20a958fc2253d6d427b
mcornell <mcornell74@gmail.com>
2014-03-17 21:43:57 -0400

    test 3 refactor if

--- bowling-game/2/src/BowlingGame.js
+++ bowling-game/2/src/BowlingGame.js
@@ -9,16 +9,20 @@ BowlingGame.prototype.roll = function(pins) {
   this.rolls[this.currentRoll++] = pins;
 };
 
+BowlingGame.prototype.isSpare = function(frameIndex) {
+  return (this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10);
+}
+
 BowlingGame.prototype.getScore = function() {
   var score = 0;
-  for (var frame = 0, i = 0; frame < 10; frame++) {
-    if (this.rolls[i] + this.rolls[i + 1] === 10) {
-      score += 10 + this.rolls[i + 2];
+  for (var frame = 0, framePtr = 0; frame < 10; frame++) {
+    if (this.isSpare(framePtr)) {
+      score += 10 + this.rolls[framePtr + 2];
     }
     else {
-      score += this.rolls[i] + this.rolls[i + 1];
+      score += this.rolls[framePtr] + this.rolls[framePtr + 1];
     }
-    i += 2;
+    framePtr += 2;
   }
 
   return score;
