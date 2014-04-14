20ee892e3aaf10de823b52ef57125235e3e29d13
mcornell <mcornell74@gmail.com>
2014-04-13 22:51:50 -0400

    solved

--- towers-of-hanoi/1/spec/GameSpec.js
+++ towers-of-hanoi/1/spec/GameSpec.js
@@ -5,16 +5,43 @@ describe("Game", function() {
     game = new Game();
   });
 
-  it("By defult has 5 disks and 3 towers", function() {
+  it("By defult has 5 disks", function() {
     expect(game.disks.length).toBe(5);
-    expect(game.towers.length).toBe(3);
   });
 
-  it("All disks start on tower 1", function() {
-    expect(game.towers[0].length).toBe(5);
+  it("All disks start on tower A", function() {
+    console.log(game.towers);
+    expect(game.towers['A'].length).toBe(5);
+    expect(game.towers['A'][0]).toBe(5);
+    expect(game.towers['A'][4]).toBe(1);
   });
 
-  describe("moving disks", function() {
-
+  it("can move a single disc from tower 'A' to 'B'", function() {
+    game.move('A', 'B');
+    expect(game.towers['A'].length).toBe(4);
+    expect(game.towers['B'].length).toBe(1);
+    expect(game.towers['B'][0]).toBe(1);
   });
+
+  it("can recursively solve the puzzle", function() {
+    game.solve();
+    expect(game.towers['A'].length).toBe(0);
+    expect(game.towers['B'].length).toBe(0);
+    expect(game.towers['C'].length).toBe(5);
+    expect(game.towers['C'][0]).toBe(5);
+    expect(game.towers['C'][4]).toBe(1);
+  });
+
+  it("can create a game with only 3 pegs and solve it", function() {
+    game = new Game(3);
+    expect(game.disks.length).toBe(3);
+    game.solve();
+    expect(game.towers['A'].length).toBe(0);
+    expect(game.towers['B'].length).toBe(0);
+    expect(game.towers['C'].length).toBe(3);
+    expect(game.towers['C'][0]).toBe(3);
+    expect(game.towers['C'][2]).toBe(1);
+  });
+
+
 });
--- towers-of-hanoi/1/src/Game.js
+++ towers-of-hanoi/1/src/Game.js
@@ -1,11 +1,42 @@
-function Game() {
-  this.disks = [1, 2, 3, 4, 5];
-  this.towers = [[], [], []];
+function Game(count) {
+  var diskCount = typeof count !== 'undefined' ? count : 5;
+  this.disks = [];
+  this.towers = {
+    'A': [],
+    'B': [],
+    'C': []
+  };
 
-  this.init = function(disks, towers) {
+  this.init = function(numdisks, disks, towers) {
+    for (var i = numdisks; i > 0; i--) {
+      disks.push(i);
+    }
     disks.forEach(function(disk) {
-      towers[0].push(disk);
+      towers['A'].push(disk);
     });
   };
-  this.init(this.disks, this.towers);
+  this.init(diskCount, this.disks, this.towers);
+};
+
+Game.prototype.toString = function() {
+  return "Tower A: " + this.towers['A'] + "\nTower B: " + this.towers['B'] + "\nTower C: " + this.towers['C'];
+};
+
+Game.prototype.move = function(from, to) {
+  var disk = this.towers[from].pop();
+  this.towers[to].push(disk);
+  console.log('moved ' + disk + ' from ' + from + ' to ' + to);
+};
+
+Game.prototype.hanoi = function(number, from, temp, to) {
+  if (number > 0) {
+    this.hanoi(number - 1, from, to, temp);
+    this.move(from, to);
+    this.hanoi(number - 1, temp, from, to);
+  }
+};
+
+Game.prototype.solve = function() {
+  this.hanoi(this.disks.length, 'A', 'B', 'C');
+  console.log("this:" + this);
 }
